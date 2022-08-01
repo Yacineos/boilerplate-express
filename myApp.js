bc72b2c93cdf66340f9437e77771c6b7e2de837e
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-08-01 20:44:45 +0200

    res.json is not a function

--- myApp.js
+++ myApp.js
@@ -1,42 +1,15 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+app.get("/json", (res, req) => {
+    res.json({ "message" : "Hello json" });
+  });
+  
 app.get("/",(req,res)=>{
-    res.send("Hello Express");
+    res.sendFile(__dirname+"/views/index.html");
 })
+app.use("/public",express.static(__dirname + "/public"));
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
