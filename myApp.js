fbe5963d90d447f4fcc23d7370a9edca54bbe4b2
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-08-02 19:45:40 +0200

    simple logger

--- myApp.js
+++ myApp.js
@@ -2,21 +2,27 @@ require('dotenv').config();
 let express = require("express");
 let app = express();
 
-
+app.use(function middelware(req,res,next){
+    var string = req.method + " " + req.path + " - " + req.ip;
+    console.log(string);
+    next();
+})
+/*
 app.get("/json", (req, res) => {
-    console.log("test");
+    res.sendFile(__dirname+"/views/index.html");
     if(process.env.MESSAGE_STYLE === "uppercase"){
-        console.log("test");
+        
         res.json({ "message" : "HELLO JSON" });
     }else{
-        console.log("test");
+
         res.json({ "message" : "Hello json" });
     }
   });
 
 app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/views/index.html");
+  
 })
+*/
 app.use("/public",express.static(__dirname + "/public"));
 
 
