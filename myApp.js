403356a2b60f9d102b23fea2c94ff140ab415e02
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-08-02 16:52:29 +0200

    hamza help

--- myApp.js
+++ myApp.js
@@ -1,15 +1,23 @@
+require('dotenv').config();
 let express = require("express");
 let app = express();
 
+
 app.get("/json", (req, res) => {
-    res.json({ "message" : "Hello json" });
+    console.log("test");
+    if(process.env.MESSAGE_STYLE === "uppercase"){
+        console.log("test");
+        res.json({ "message" : "HELLO JSON" });
+    }else{
+        console.log("test");
+        res.json({ "message" : "Hello json" });
+    }
   });
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/views/index.html");
 })
 app.use("/public",express.static(__dirname + "/public"));
-
 
 
 module.exports = app;
