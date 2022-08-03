1062e2f86e657622f2329d2cbca38b9e5070c82a
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-08-03 11:16:04 +0200

    done

--- myApp.js
+++ myApp.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 let express = require("express");
 let app = express();
-
+/*
 app.use(function middelware(req,res,next){
     var string = req.method + " " + req.path + " - " + req.ip;
     console.log(string);
     next();
 })
+*/
+app.get("/now",(req,res,next)=> {
+  req.time = new Date().toString();  
+  next();  
+},
+(req,res)=>{
+    res.send({time: req.time})
+}
+)
 /*
 app.get("/json", (req, res) => {
     res.sendFile(__dirname+"/views/index.html");
